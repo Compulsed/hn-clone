1baed28d47219f110dba9c38a834ee6fae1c77dc
Dale Salter <dalejsalter@outlook.com>
2017-09-28 15:31:35 +1000

    WIP: State of comments

--- src/bff/model/Comment.ts
+++ src/bff/model/Comment.ts
@@ -1,39 +1,35 @@
-import * as DataLoader from 'dataloader';
-import * as _ from 'lodash';
+// import * as DataLoader from 'dataloader';
+// import * as _ from 'lodash';
 
-import { Author } from './Author';
-import { getLinks, getUsers } from '../lambda';
+// import { Author } from './Author';
+// import { getLinks, getUsers } from '../lambda';
 
 
-export const Link = `
-    type Link {
-        linkId: String!
-        authorId: String!
-        title: String!
-        author: Author!
-    }
-`;  
+// export const Link = `
+//     type Link {
+//         linkId: String!
+//         authorId: String!
+//         title: String!
+//         author: Author!
+//     }
+// `;  
 
-export const Loaders = () => ({
-    linkLoader: new DataLoader(
-        linkIds => getLinks(linkIds)
-    ),
-    
-    userLoader: new DataLoader(
-        userIds => getUsers(userIds)
-    ),
-});
+// export const Loaders = () => ({
+//     linkLoader: new DataLoader(
+//         linkIds => getLinks(linkIds)
+//     ),
+// });
   
-export const Resolvers = {
-    Link: {
-        author: ({ authorId }, args, { userLoader }) => 
-            userLoader.load(authorId),
-    },
+// export const Resolvers = {
+//     Link: {
+//         author: ({ authorId }, args, { userLoader }) => 
+//             userLoader.load(authorId),
+//     },
 
-    Query: {
-        comment: (root, { commentId }, { commentLoader }) =>
-            linkLoader.load(linkId),
-    },
-};
+//     Query: {
+//         comment: (root, { commentId }, { commentLoader }) =>
+//             linkLoader.load(linkId),
+//     },
+// };
 
-export const LinkType = () => [Link];
+// export const LinkType = () => [Link];
